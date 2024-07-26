e63abe1a2be7869805ad258c4c4b2422953330fa
dilaraydus <dilaraydus@gmail.com>
2024-07-26 21:03:55 +0300

    Started Test on Netflix

---
+++ cypress/e2e/user-tests/login-tests.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('Login Test', () => {
+
+    it('Login Succesessful', () => {
+        // https://on.cypress.io/focus
+        cy.visit("https://www.netflix.com/login")
+      })
+
+})
